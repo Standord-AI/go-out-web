e1d355835d8d2b69e7e6dcbcaa1d67927a4d0e76
ShakthiW <shakthi.20221274@iit.ac.lk>
2025-03-30 08:23:46 +0530

    Update logo dimensions in login and signup pages for consistency

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="/" className="flex items-center gap-2 self-center font-medium">
-          <Image src="/logo/logo-long.png" alt="GoOut Logo" width={100} height={100} />
+          <Image src="/logo/logo-long.png" alt="GoOut Logo" width={100} height={50} />
         </a>
         <SignupForm />
       </div>
