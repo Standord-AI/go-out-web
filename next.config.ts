16816161dc47e8b5d22353e19245f70a1b62cd38
Tharusha Kulasinghe <tharusha22589@gmail.com>
2025-07-29 18:36:01 +0530

    Add new remote image pattern for Firebase storage in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
